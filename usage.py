4ce219a97512dbb2d7e1d6d14c4a6363a8516d90
Nick DeRobertis <whoopnip@gmail.com>
2020-05-16 07:23:39 -0400

    Dash app was not working because list of files was wrapped in tuple

--- usage.py
+++ usage.py
@@ -3,19 +3,17 @@ import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, show_undo_redo=True)
 
-EXAMPLE_FILES = (
-    [
-        {"key": "photos/animals/cat in a hat.png", "size": 1.5 * 1024 * 1024,},
-        {"key": "photos/animals/kitten_ball.png", "size": 545 * 1024,},
-        {"key": "photos/animals/elephants.png", "size": 52 * 1024,},
-        {"key": "photos/funny fall.gif", "size": 13.2 * 1024 * 1024,},
-        {"key": "photos/holiday.jpg", "size": 85 * 1024,},
-        {"key": "documents/letter chunks.doc", "size": 480 * 1024,},
-        {"key": "documents/export.pdf", "size": 4.2 * 1024 * 1024,},
-    ],
-)
+EXAMPLE_FILES = [
+    {"key": "photos/animals/cat in a hat.png", "size": 1.5 * 1024 * 1024,},
+    {"key": "photos/animals/kitten_ball.png", "size": 545 * 1024,},
+    {"key": "photos/animals/elephants.png", "size": 52 * 1024,},
+    {"key": "photos/funny fall.gif", "size": 13.2 * 1024 * 1024,},
+    {"key": "photos/holiday.jpg", "size": 85 * 1024,},
+    {"key": "documents/letter chunks.doc", "size": 480 * 1024,},
+    {"key": "documents/export.pdf", "size": 4.2 * 1024 * 1024,},
+]
 
 app.layout = html.Div(
     [
